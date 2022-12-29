8bd141e43a07532c3152ea087be1711eda9b30c1
brthurr <11654145+brthurr@users.noreply.github.com>
2022-12-29 16:45:17 -0600

    new file:   .p10k.zsh 	new file:   pl10kconfig.cfg 	modified:   setup.sh 	modified:   setupLibrary.sh

--- setup.sh
+++ setup.sh
@@ -21,10 +21,29 @@ function main() {
     logTimestamp "${output_file}"
     exec 3>&1 >>"${output_file}" 2>&1
 
+    echo "Installing and configuring ZSH/ZSH-OMG and Powerlevel10k " >&3
+    setupZsh
+
+    echo "Upgrading server... " >&3
+    updateServer
+    
     echo "Configuring System Time... " >&3
     setupTimezone
 
-    echo "Setup Done! Log file is located at ${output_file}" >&3
+    echo "Install pyenv... " >&3
+    setupPyEnv
+
+    read -rp "Do you want to change your shell to zsh now? (Recommended) [Y/N] " changeShell
+
+    if [[ $changeShell == [yY] ]]; then
+        sudo chsh -s $(which zsh) $(whoami)
+    else
+        echo 'This is not a valid choice!'
+        exit 1
+    fi
+
+    echo "Setup Done! Log file is located at ${output_file}. Logout and back in to use zsh." >&3
+
 }
 
 function logTimestamp() {
--- setupLibrary.sh
+++ setupLibrary.sh
@@ -3,6 +3,15 @@
 # Setup the ZSH
 function setupZsh() {
     sudo apt-get install zsh
+    yes y | sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+    echo -e "$(cat pl10kconfig.cfg)\n\n$(cat ~/.zshrc)" > ~/.zshrc
+    sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/gi' ~/.zshrc
+    echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc
+    cp .p10k.zsh ~/.p10k.zsh
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    sed -i 's/(git)/(git zsh-syntax-highlighting zsh-autosuggestions)/gi' ~/.zshrc
 }
 
 # Set the machine's timezone
@@ -12,3 +21,18 @@ function setTimezone() {
     local timezone=${1}
     sudo timedatectl set-timezone ${timezone}
 }
+
+function updateServer() {
+    sudo apt-get update
+    sudo apt-get -y upgrade
+}
+
+function setupPyEnv() {
+    sh -c "$(curl https://pyenv.run)"
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+    echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.zshrc
+}
+
+function changeShell()
